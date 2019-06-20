892cdc78ba82f9fb7daf6538422a27cbfe52860e
Richard Jamieson <richard.jamieson@sscsuk.net>
2019-06-21 00:01:05 +0100

    --restart=always

--- docker/run_container.sh
+++ docker/run_container.sh
@@ -4,9 +4,10 @@ docker volume create scs_nginx_certs
 
 # Need to update the certificates dir first !
 
-docker run -d --name=scs_nginx --network=jamnet --hostname=scs_nginx --publish 80:80 --publish 443:443 \
+docker run -d --name=scs_nginx --network=jamnet --hostname=scs_nginx --restart=always \
+--publish 80:80 --publish 443:443 \
 -v scs_nginx_config:/etc/opt/rh/rh-nginx18/nginx \
 -v scs_nginx_certs:/mnt/certs \
 -v scs_nginx_logs:/var/opt/rh/rh-nginx18/log/nginx \
---restart=always localhost:5000/scsuk.net/scs_nginx:1.0
+localhost:5000/scsuk.net/scs_nginx:1.0
 
