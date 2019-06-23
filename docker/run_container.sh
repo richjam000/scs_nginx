dfa3365c366ad65b573d04921114bb8a623ef8e0
Richard Jamieson <richard.jamieson@sscsuk.net>
2019-06-23 21:31:22 +0100

    No need to create volumes in separate command

--- docker/run_container.sh
+++ docker/run_container.sh
@@ -1,13 +1,9 @@
-docker volume create scs_nginx_config
-docker volume create scs_nginx_logs
-docker volume create scs_nginx_certs
-
-# Need to update the certificates dir first !
-
 docker run -d --name=scs_nginx --network=jamnet --hostname=scs_nginx --restart=always \
 --publish 80:80 --publish 443:443 \
 -v scs_nginx_config:/etc/opt/rh/rh-nginx18/nginx \
 -v scs_nginx_certs:/mnt/certs \
 -v scs_nginx_logs:/var/opt/rh/rh-nginx18/log/nginx \
 localhost:5000/scsuk.net/scs_nginx:1.0
+
+echo "Now need to install the SSL certificates into volume scs_nginx_config"
 
